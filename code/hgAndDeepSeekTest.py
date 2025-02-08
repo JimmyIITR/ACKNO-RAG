d3da4b993b014415be7632f83a3a60775ae8801b
JimmyIITR <jimmy_ca@cs.iitr.ac.in>
2025-02-08 23:26:42 +0530

    deepseek test 1

---
+++ code/hgAndDeepSeekTest.py
@@ -0,0 +1,11 @@
+import huggingface_hub
+import transformers
+import accelerate
+import bitsandbytes
+import langchain
+import langchain_core
+import langchain_community
+import langchain_ollama
+import langchain_openai
+
+
